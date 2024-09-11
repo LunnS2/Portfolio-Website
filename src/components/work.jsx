48a2f3ef7b03eb77defd165d1b5b7891aa35fc8c
Lunn <thafm.work@gmail.com>
2024-09-11 22:27:50 +0100

    Update work section

--- src/components/work.jsx
+++ src/components/work.jsx
@@ -2,22 +2,43 @@ import React from "react";
 
 function Work() {
   return (
-    <section id="work" className="min-h-screen p-6 sm:p-12 md:p-24 flex flex-col items-center text-black dark:text-white bg-gray-100 dark:bg-gray-900 transition-colors duration-300">
-      <h2 className="text-3xl font-bold mb-8 sm:text-3xl sm:mb-8 ">Work</h2>
-      <p className="mb-8 sm:mb-12 text-center text-lg">Content for the Work section.</p>
+    <section
+      id="work"
+      className="min-h-screen p-6 sm:p-12 md:p-24 flex flex-col items-center text-black dark:text-white bg-gray-100 dark:bg-gray-900 transition-colors duration-300"
+    >
+      <h2 className="text-3xl font-bold mb-8 sm:text-3xl sm:mb-8">Work</h2>
+      <p className="mb-8 sm:mb-12 text-center text-lg">
+        Content for the Work section.
+      </p>
       <div className="flex flex-wrap gap-4 justify-center">
-        <div className="w-full sm:w-64 h-64 bg-white text-black border-4 border-slate-200 hover:bg-slate-200 flex items-center justify-center rounded-lg">
+        <a
+          href="https://github.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="w-full sm:w-64 h-64 bg-white text-black border-4 border-slate-200 hover:bg-slate-200 flex items-center justify-center rounded-lg"
+        >
           GitHub Project 1
-        </div>
-        <div className="w-full sm:w-64 h-64 bg-white text-black border-4 border-slate-200 hover:bg-slate-200 flex items-center justify-center rounded-lg">
+        </a>
+        <a
+          href="https://github.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="w-full sm:w-64 h-64 bg-white text-black border-4 border-slate-200 hover:bg-slate-200 flex items-center justify-center rounded-lg"
+        >
           GitHub Project 2
-        </div>
-        <div className="w-full sm:w-64 h-64 bg-white text-black border-4 border-slate-200 hover:bg-slate-200 flex items-center justify-center rounded-lg">
+        </a>
+        <a
+          href="https://github.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="w-full sm:w-64 h-64 bg-white text-black border-4 border-slate-200 hover:bg-slate-200 flex items-center justify-center rounded-lg"
+        >
           GitHub Project 3
-        </div>
+        </a>
       </div>
     </section>
   );
 }
 
 export default Work;
+
