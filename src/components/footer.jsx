bd5a0ca83b4375bc2808307928d84e246477755b
Lunn <thafm.work@gmail.com>
2025-04-10 20:30:35 +0100

    Update footer's phrasing

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <div className="bg-white text-black dark:bg-black dark:text-white p-2 w-full top-0 left-0 z-50 text-center transition-colors duration-300">
-      <p>Copyright ⓒ {year}</p>
+      <p>ⓒ {year} All rights reserved.</p>
     </div>
   ) 
 }
