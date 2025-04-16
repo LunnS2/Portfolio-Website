7aaf57a832fd8ca70865192e4f1f29388c7eca81
Lunn <thafm.work@gmail.com>
2025-04-16 19:24:12 +0100

    Update footer's padding

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   const year = new Date().getFullYear();
   return (
-    <div className="bg-white text-black dark:bg-black dark:text-white p-8 w-full top-0 left-0 z-50 text-center transition-colors duration-300">
+    <div className="bg-white text-black dark:bg-black dark:text-white p-2 w-full top-0 left-0 z-50 text-center transition-colors duration-300">
       <p>ⓒ {year} All rights reserved.</p>
     </div>
   ) 
